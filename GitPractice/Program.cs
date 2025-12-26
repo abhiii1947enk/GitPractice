fd7b1a8e5e0bfd1cbcad9e6ee5da3cc005b61ae9
Abhishek Nigam <abhishek.n@enktechnologies.com>
2025-12-27 02:40:44 +0530

    new commit

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -1,4 +1,4 @@
-"﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace GitPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Git!");
-            Console.WriteLine("Hello Git - Second Commit");  
+            Console.WriteLine("Hello Git - Second Commit");
             Console.WriteLine("Hello Git - Third Commit");
             Console.WriteLine("Learning Git Diff");
             Console.WriteLine("Feature branch Commit");
